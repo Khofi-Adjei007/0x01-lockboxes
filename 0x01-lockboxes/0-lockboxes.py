fdad78df2cfabb3cd7257d032eb3098e642d1682
Khofi-Adjei007 <khofiadjei@gmail.com>
2024-06-09 16:40:40 -0700

    Seperated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -23,14 +23,3 @@ def canUnlockAll(boxes):
                 stack.append(key)
 
     return len(unlocked) == n
-
-
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # False
