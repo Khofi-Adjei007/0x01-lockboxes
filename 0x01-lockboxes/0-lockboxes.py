495b823b09ba6e12a2127022c325e157bcaff8ab
Khofi-Adjei007 <khofiadjei@gmail.com>
2024-06-09 16:47:44 -0700

    Figditing Around

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-
     """
     Determine if all the boxes can be opened.
     Parameters:
--- 0x01-lockboxes/main_0.py
+++ 0x01-lockboxes/main_0.py
@@ -1,12 +1,26 @@
 #!/usr/bin/python3
+"""
+This script tests the functionality of
+the canUnlockAll function,
+which checks if all boxes can be unlocked given a
+list of lists where each sublist
+contains keys to other boxes.
+
+The script imports the canUnlockAll function from the
+module '0-lockboxes' and tests it
+with three different sets of boxes.
+"""
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
+# Test case 1: Sequential keys
 boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
+print(canUnlockAll(boxes))  # Expected output: True
 
+# Test case 2: Multiple keys and cycles
 boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
+print(canUnlockAll(boxes))  # Expected output: True
 
+# Test case 3: Disconnected keys
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+print(canUnlockAll(boxes))  # Expected output: False
